6a49835a94934b37c1db243c9e85c7f0b6d89eb3
Cezar Augusto Mezzalira <cezar.mezzalira@gmail.com>
2024-05-20 21:26:36 -0300

    hotfix

--- src/NovaClasse.java
+++ src/NovaClasse.java
@@ -1,5 +1,12 @@
 public class NovaClasse {
     public static void main(String[] args) {
         System.out.println("Olá Pessoal");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("Aula de git");
+        System.out.println("Nome: Joaquim");
     }
 }
