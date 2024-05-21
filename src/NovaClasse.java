ee821645a197aef63373e98584cdb4e66eafc342
Cezar Augusto Mezzalira <cezar.mezzalira@gmail.com>
2024-05-20 21:29:38 -0300

    Imprime alguma coisa que explode

--- src/NovaClasse.java
+++ src/NovaClasse.java
@@ -1,5 +1,13 @@
 public class NovaClasse {
     public static void main(String[] args) {
         System.out.println("Olá Pessoal");
+
+        while (true) {
+            break;
+        }
+
+        System.out.println("Aula de git");
+        String nome = "Joaquim";
+        System.out.println("Nome: " + nome);
     }
 }
