60491da4b63393859233c30836ab232a0180ab8c
Cezar Augusto Mezzalira <cezar.mezzalira@gmail.com>
2024-05-27 19:29:45 -0300

    feat: criada nova funcionalidade

---
+++ src/NovaFuncionalidade.java
@@ -0,0 +1,7 @@
+public class NovaFuncionalidade {
+    public static void main(String[] args) {
+
+        System.out.println("Olá Pessoal");
+        System.out.println("Funcionalidade nova");
+    }
+}
